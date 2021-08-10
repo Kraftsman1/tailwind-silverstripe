0327ece62d7242bf5df17ca494ee61e808eb8e81
Asigri Shamsu-Deen Al-Heyr <shamsasigri@gmail.com>
2021-08-10 17:14:47 +0000

    make it better

--- app/_config.php
+++ app/_config.php
@@ -8,17 +8,3 @@ $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
 
-//Heroku ClearDB support
-if(isset($_ENV['CLEARDB_DATABASE_URL'])) {
-	global $databaseConfig;
-	$parts = parse_url($_ENV['CLEARDB_DATABASE_URL']);
-	$databaseConfig['type'] = 'MySQLPDODatabase';
-	$databaseConfig['server'] = $parts['host'];
-	$databaseConfig['username'] = $parts['admin'];
-	$databaseConfig['password'] = $parts['password'];
-	$databaseConfig['database'] = trim($parts['path'], '/');
-	Security::setDefaultAdmin('heroku', 'yesletmeinplease');
-} else {
-	//Default SilverStripe environement support
-	require_once('conf/ConfigureFromEnv.php');
-}
